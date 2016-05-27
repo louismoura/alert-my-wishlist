7d028bc09de0a18016b30c34faaa6968f7874d8b
King <kingsleywang2013@gmail.com>
2016-05-27 10:00:28 +1000

    Add .gitignore file

--- src/models/alerts/constants.py
+++ src/models/alerts/constants.py
@@ -1,7 +1,7 @@
+import os
 
-
-URL = "https://api.mailgun.net/v3/sandboxdca31cc55dae4d53abfd36982582a773.mailgun.org/messages"
-API_KEY = "key-cba58cff0ccbea95315b7214da2f2261"
-FROM = "Mailgun Sandbox <postmaster@sandboxdca31cc55dae4d53abfd36982582a773.mailgun.org>"
+URL = os.environ.get('MAILGUN_URL')
+API_KEY = os.environ.get('MAILGUN_API_KEY')
+FROM = os.environ.get('MAILGUN_FROM')
 ALERT_TIMEOUT = 1
 COLLECTION = "alerts"
